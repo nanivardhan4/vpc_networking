98f07ac9bcaa34958783e161f0585016e6b30bb4
nanivardhan <nanivardhan4@gmail.com>
2024-12-23 11:24:42 +0530

    third change

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ vpc_id = aws_vpc.main.id
 cidr_block = "10.0.1.0/24"
 availability_zone = "us-west-1a"
 }
+resource "aws_internet_gateway" "igw" {
+vpc_id = aws_vpc.main.id
+}
+resource "aws_route_table" "route_table" {
+vpc_id = aws_vpc.main.id
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.igw.id
+}
+}
