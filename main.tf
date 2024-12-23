81ab7bd314c3142cb417ebe4e9c1ecb0badad1f4
nanivardhan <nanivardhan4@gmail.com>
2024-12-23 11:18:31 +0530

    second change

--- main.tf
+++ main.tf
@@ -4,3 +4,8 @@ region = "us-west-1"
 resource "aws_vpc" "main" {
 cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "subnet" {
+vpc_id = aws_vpc.main.id
+cidr_block = "10.0.1.0/24"
+availability_zone = "us-west-1a"
+}
